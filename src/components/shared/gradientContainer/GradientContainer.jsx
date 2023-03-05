4e75567fc4c8252fe4e4a9e9a76be14cab41409a
Valentin Gonzalez <valentingonzalezlou@gmail.com>
2023-03-05 15:57:43 -0300

    font and texts

--- src/components/shared/gradientContainer/GradientContainer.jsx
+++ src/components/shared/gradientContainer/GradientContainer.jsx
@@ -2,8 +2,10 @@ import "./_GradientContainer.scss";
 
 export default function GradientContainer({ title }) {
   return (
-    <div className="blur-out-contract title--container">
-      <h1>{title}</h1>
+    <div className="blur-out-contract">
+      <div className="title--container">
+        <h1>{title}</h1>
+      </div>
     </div>
   );
 }
